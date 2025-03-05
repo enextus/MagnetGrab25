127e3bd3c61a92e078dd80079ef80c9f0297fe72
Eduard <36194778+enextus@users.noreply.github.com>
2025-03-05 07:46:58 +0100

    opt

--- src/main/java/org/image/ImgProvider.java
+++ src/main/java/org/image/ImgProvider.java
@@ -67,27 +67,4 @@ public class ImgProvider {
 		}
 	}
 
-	public static void ReadingAndPrintingBytes(InputStream inputStream) {
-		// Reading and printing bytes to the console
-		try {
-			int byteRead;
-			while ((byteRead = inputStream.read()) != -1) {
-				System.out.print(byteRead + " : ");
-			}
-		}
-		catch (IOException e) {
-			e.printStackTrace();
-		}
-		finally {
-			// close InputStream
-			try {
-				inputStream.close();
-			}
-			catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-
-	}
-
 }
